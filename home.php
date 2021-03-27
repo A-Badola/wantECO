1e2af4cb1d740aef319eaa291c1d9f6d75341a25
web-advisor <75082566+web-advisor@users.noreply.github.com>
2021-03-28 02:23:30 +0530

    Font icons updated

---
+++ home.php
@@ -0,0 +1,158 @@
+<section class="slider">
+    <div class="slide-viewer">
+      <div class="slide-group">
+        <div class="slide slide-1">
+          <img src="img/slide-1.jpg" alt="1 slide" />
+        </div>
+        <div class="slide slide-2">
+          <img src="img/slide-2.jpg" alt="2 slide" />
+        </div>
+        <div class="slide slide-1">
+          <img src="img/slide-3.jpg" alt="3 slide" />
+        </div>
+        <div class="slide slide-1">
+          <img src="img/slide-4.jpg" alt="4 slide" />
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--- main part: for get and provide part -->
+  <div class="slide-buttons"></div>
+  <section class="hero">
+    <div class="container" >
+      <div class="hero__text container--pall">
+        <?php if(isset($_SESSION['id'])){ ?>
+          <h1>
+              Let's hold OUR WORLD in trust for OUR CHILDREN .
+          </h1>
+          <p>
+              Explore the community or Contribute to the communtiy of Sustainable Living 
+          </p>
+        <?php }else{   ?>
+          <h1>
+              Live More Sustainably<br >Join our wantECO community
+          </h1>
+          <p>
+              join over `number` people using ways to live more sustainably, talking about their daily eco-friendly and sustainable methods and options, inspiring each other and chatting about all things sustainable! 
+          </p>
+        <?php  }  ?>   
+
+       <a href="?page=explore" class="button hero__cta">EXPLORE</a>
+       <a href="?page=contribute" class="button hero__cta">CONTRIBUTE</a>
+      </div>
+    </div>
+  </section>
+  <!--- for that trending plastic use sustainable ideas waste management part-->
+  <section class="feature ">
+    <div class="feature__content container container--pall">
+      <div class="feature__intro">
+        <h2>Find Out</h2>
+        <p>
+          what are our members daily eco-friendly and sustainable methods and options are.
+        </p>
+      </div>
+  
+      <div class="feature__grid">
+        <div class="feature__item">
+        <a href="#">
+            <i class="fa fa-newspaper-o" aria-hidden="true" class="feature__icon"></i>
+          <div class="feature__title">
+            Trending
+          </div>
+          <div class="feature__description">
+            Trending stories and articles that allow you to keep track of your unsustainable methods and daily activities and give you insight about how to tackle them easily.
+          </div>
+        </div>
+  
+        <div class="feature__item">
+        <a href="#">
+            <i class="fa fa-home" aria-hidden="true" class="feature__icon"></i>
+          <div class="feature__title">
+            Plastic-free products and households.
+          </div>
+          <div class="feature__description">
+            See exactly where your money goes each month which indirectly have climate cost too. See replacement of those products and many more...
+          </div>
+        </div>
+  
+        <div class="feature__item">
+        <a href="#">
+            <i class="fa fa-lightbulb-o" aria-hidden="true" class="feature__icon"></i>
+          <div class="feature__title">
+            Ideas
+          </div>
+          <div class="feature__description">
+            Ideas and suggesstions about lifestyle changes both at market and individual or consumer level can be the key to healthy environment. 
+          </div>
+        </div>
+  
+        <div class="feature__item">
+        <a href="#">
+            <i class="fa fa-trash" aria-hidden="true" class="feature__icon"></i>
+          <div class="feature__title">
+            Waste Management
+          </div>
+          <div class="feature__description">
+            Check out some better option to reducee, recycle, reuse and dispose waste products of different kinds.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--articles to view to fill up the page ,We can put it in slider also if needed-->
+  <section class="article">
+    <div class="article__content container container--pall">
+      <h2>Latest Articles</h2>
+
+      <div class="article__grid">
+        <a href="#" class="article__item">
+          <div class="article__image" style="background-image: url('/images/img1.jpg')"></div>
+          <div class="article__text">
+            <div class="article__author">By Ace1</div>
+            <div class="article__title">article-title-1</div>
+            <div class="article__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error nostrum eius ipsam voluptatem dolorum accusantium fugiat repudiandae sapiente, ratione delectus numquam magni quis laboriosam est minus illum repellendus. Adipisci, odio!...</div>
+          </div>
+        </a>
+        <a href="#" class="article__item">
+          <div class="article__image" style="background-image: url('/images/img2.jpg')"></div>
+          <div class="article__text">
+            <div class="article__author">By Ace2</div>
+            <div class="article__title">article-title-2
+            </div>
+            <div class="article__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae molestias, porro modi quas at perferendis beatae aliquam cum sapiente, ducimus libero sequi ea, architecto nostrum eligendi perspiciatis adipisci suscipit error?... 
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="article__item">
+          <div class="article__image" style="background-image: url('/images/img3.jpg')"></div>
+          <div class="article__text">
+            <div class="article__author">By Ace3</div>
+            <div class="article__title">article-title-3</div>
+            <div class="article__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolor est accusamus ipsum rerum necessitatibus ex optio, voluptatibus id doloribus non cupiditate vero magni! Placeat nobis vero laudantium temporibus molestiae?...
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="article__item">
+          <div class="article__image" style="background-image: url('/images/img4.jpg')"></div>
+          <div class="article__text">
+            <div class="article__author">By Ace4</div>
+            <div class="article__title">article-title-4</div>
+            <div class="article__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores iure sit modi ipsam ad aliquid provident officiis debitis impedit odit id, minima earum placeat quas consequuntur? Tempora quas suscipit similique...
+            </div>
+          </div>
+        </a>      
+      </div>
+    </div>
+  </section>
+  <section class="carbonFootprintCalc mx-auto w-75 mb-4" style="display: table;">
+    <h2>Calculate your carbon emissionss</h2>
+    <iframe
+      width="100%"
+      height="500px"
+      src="https://calculator.carbonfootprint.com/calculator.aspx?b=99CCFF"
+    >
+    </iframe>
+  </section>
