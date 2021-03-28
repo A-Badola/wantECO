939b5852c815cdcfee7bca40711a1504fe0ce7e5
web-advisor <75082566+web-advisor@users.noreply.github.com>
2021-03-28 16:54:22 +0530

    explore section backend introduced

--- Views/explore.php
+++ Views/explore.php
@@ -1,5 +1,6 @@
 <?php
-
+  $sql="SELECT * from `posts`";
+  $result=$link->query($sql);
 ?>
 
 <!-- Just to put some introduction , aaa, and bcoz still dont know what to add-->
@@ -26,24 +27,11 @@
   <section class="tagsTale">
     <div id="searchTags"></div>
         <div class="tagsTale__tagnames" id="serviceList">
-          <a href="" class="serviceDesc" data-tags="waste-management,recycle,popular"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
-          <a href="" class="serviceDesc" data-tags="waste-management,recycle"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
-          <a href="" class="serviceDesc" data-tags="waste-management,recycle"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
-          <a href="" class="serviceDesc" data-tags="waste-management"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
-          <a href="" class="serviceDesc" data-tags="web Design,web"><h3>SaveAt-Web</h3><p>An approach to use web with less carbon emissions... </p></a>
-          <a href="" class="serviceDesc" data-tags="web Design,web"><h3>SaveAt-Web</h3><p>An approach to use web with less carbon emissions... </p></a>
-          <a href="" class="serviceDesc" data-tags="web Design"><h3>SaveAt-Web</h3><p>An approach to use web with less carbon emissions... </p></a>
-          <a href="" class="serviceDesc" data-tags="sustainable rides"><h3>GreenRides</h3><p>A service where you can hire a bicycle from a stand close to you and drop it at any stand within the journey... </p></a>
-          <a href="" class="serviceDesc" data-tags="sustainable rides,PublicTransport"><h3>GreenRides</h3><p>A service where you can hire a bicycle from a stand close to you and drop it at any stand within the journey... </p></a>
-          <a href="" class="serviceDesc" data-tags="popular,sustainable rides"><h3>GreenRides</h3><p>A service where you can hire a bicycle from a stand close to you and drop it at any stand within the journey... </p></a>
-          <a href="" class="serviceDesc" data-tags="Logistics"><h3>GreenRides</h3><p>Mission in mind to make your Logistics greener and more sustainable... </p></a>
-          <a href="" class="serviceDesc" data-tags="popular,Logistics"><h3>GreenRides</h3><p>Mission in mind to make your Logistics greener and more sustainable... </p></a>
-          <a href="" class="serviceDesc" data-tags="Logistics"><h3>GreenRides</h3><p>Mission in mind to make your Logistics greener and more sustainable... </p></a>
-          <a href="" class="serviceDesc" data-tags="Textile"><h3>Sustainable Textile Solutions</h3><p>Dedicated to assisting brands, retailers and industry partneres implement eco-requirements for sustainable production... </p></a>
-          <a href="" class="serviceDesc" data-tags="Textile"><h3>Sustainable Textile Solutions</h3><p>Dedicated to assisting brands, retailers and industry partneres implement eco-requirements for sustainable production... </p></a>
-          <a href="" class="serviceDesc" data-tags="Textile,popular"><h3>Sustainable Textile Solutions</h3><p>Dedicated to assisting brands, retailers and industry partneres implement eco-requirements for sustainable production... </p></a>
-          <a href="" class="serviceDesc" data-tags="waste-management,e-waste"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
-          <a href="" class="serviceDesc" data-tags="waste-management,e-waste"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a>
+        <!-- <a href="" class="serviceDesc" data-tags="waste-management,recycle,popular"><h3>DumpIn</h3><p>We are leading firm in Recycling Sector prooviding different services to our clients...</p></a> -->
+
+          <?php while($row=$result->fetch_assoc()){ ?>
+            <a href="" class="serviceDesc" data-tags="<?php echo $row['category']; ?>"><h3><?php echo $row['category']; ?></h3><p><?php echo $row['post']; ?></p></a>
+          <?php } ?>
         </div>
     </div>
   </section>
